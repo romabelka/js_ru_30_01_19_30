7feb2b519e3ff54bc3db314b618fe80d96b5b21e
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-02-02 19:21:25 +0200

    add Article component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+import React from 'react'
+import {render} from 'react-dom'
+import Article from './Article'
+import {articles} from './fixtures'
+
+render(<Article article={articles[0]} />, document.getElementById('container'))
