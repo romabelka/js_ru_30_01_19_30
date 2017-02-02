febdb430ef021210b56ad76171b1511ece92c923
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-02-02 19:37:50 +0200

    add Article list

---
+++ src/ArticleList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Article from './Article'
+
+export default function ArticleList(props) {
+    const {articles} = props
+    const articleElements = articles.map((article) => <li key={article.id}><Article article={article}/></li>)
+    return (
+        <ul>
+            {articleElements}
+        </ul>
+    )
+}
