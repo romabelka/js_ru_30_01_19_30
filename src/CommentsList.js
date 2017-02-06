2115452086e101a2a9b0721e7ccb1cd41ecd794e
Roman <kaboom.ua@gmail.com>
2017-02-06 12:12:19 +0200

    Update CommentsList.js

--- src/CommentsList.js
+++ src/CommentsList.js
@@ -5,6 +5,7 @@ import Comments from './Comments'
 export default class CommentsList extends Component {
   render(){
     let { article: { comments } } = this.props
+    //откуда comments.id, это же массив?
     const commentsElements = comments.map((comments) => <div key={comments.id}><Comments comments={comments}/></div>)
     return ( <ul>{commentsElements}</ul> )
   }
