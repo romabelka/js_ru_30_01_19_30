5ad0ac268461df4dafec274ec1216cd9481b5830
s.sumatokhin <s.sumatokhin@rambler-co.ru>
2017-02-08 09:35:09 +0300

    decorator

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,12 +4,13 @@ import toggleOpen from '../decorators/toggleOpen'
 
 function ArticleList (props) {
 
-    const {articles, toggleOpenArticle} = props
+
+    const {articles, toggleOpen, openArticleId} = props
     const articleElements = articles.map((article) => <li key={article.id}>
         <Article
             article={article}
-            isOpen={article.id == state.openArticleId}
-            toggleOpen={toggleOpenArticle(article.id)}/>
+            isOpen={article.id == openArticleId}
+            toggleOpen={toggleOpen(article.id)}/>
     </li>)
     return (
         <ul>
