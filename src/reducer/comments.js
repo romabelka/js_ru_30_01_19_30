86d33e850517fccef1fe6f7babe8ffcc17e9b403
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-02-16 19:13:25 +0200

    add comments reducer

---
+++ src/reducer/comments.js
@@ -0,0 +1,11 @@
+import {normalizedComments as defaultComments} from '../fixtures'
+
+export default (state = defaultComments, action) => {
+    const {type, payload} = action
+
+    switch (type) {
+
+    }
+
+    return state
+}
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,8 +2,9 @@ import {combineReducers} from 'redux'
 import counterReducer from './counter'
 import articles from './articles'
 import filters from './filters'
+import comments from './comments'
 
 export default combineReducers({
     count: counterReducer,
-    articles, filters
+    articles, filters, comments
 })
