b7c64e8d1eb7e9a3af9c345bcc7b075551288135
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-03-02 18:51:36 +0200

    add index routes and redirects

---
+++ src/routeHandlers/ArticleIndexPage.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+class ArticleIndexPage extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Select Article</h1>
+            </div>
+        )
+    }
+}
+
+export default ArticleIndexPage
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Router, Route, hashHistory, browserHistory} from 'react-router'
+import {Router, Route, IndexRoute, Redirect, IndexRedirect, hashHistory, browserHistory} from 'react-router'
 import Root from './components/Root'
 import ArticleList from './routeHandlers/ArticleList'
 import Filters from './routeHandlers/Filters'
@@ -10,12 +10,17 @@ import AuthorizedOnlyPage from './routeHandlers/AuthorizedOnlyPage'
 import ErrorPage from './routeHandlers/ErrorPage'
 import CommentsRoot from './RouteHandlers/CommentsRoot'
 import CommentsPage from './RouteHandlers/CommentsPage'
+import NewArticle from './RouteHandlers/NewArticle'
+import ArticleIndexPage from './RouteHandlers/ArticleIndexPage'
 import store from './store'
 
 export default (
     <Router history={browserHistory}>
         <Route path="/" component={Root}>
+            <IndexRedirect to="/articles"/>
             <Route path="articles" component={ArticleList}>
+                <IndexRoute component={ArticleIndexPage}/>
+                <Route path="/new" component={NewArticle} />
                 <Route path=":id" component={ArticlePage} />
             </Route>
             <Route path="filters" component={Filters}/>
@@ -28,7 +33,11 @@ export default (
                     }
                 }
             />
+            <Redirect from="comments" to="comments/1"/>
             <Route path = "comments" component = {CommentsRoot}>
+{/*
+                <IndexRedirect to="1" />
+*/}
                 <Route path = ":page" component = {CommentsPage} />
             </Route>
             <Route path="error" component={ErrorPage} />
